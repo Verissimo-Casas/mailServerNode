4b11303fdc4427548bf35fb8a00433d958136019
Veríssimo Casas <Verissimo0073@gmail.com>
2022-10-03 21:25:16 -0400

    create src/controlles/UseController.ts

---
+++ src/controlles/UseController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import Mailservice from "../services/Mailservice";
+
+interface CreateUserBody{
+  name: string; 
+  email: string;
+}
+
+class UseController {
+  findAll(request: Request, response: Response){
+    return response.json({
+      users: [
+        {
+          name: "John Doe",
+          email: "JohnDoe@rail.com"
+        },
+      ],
+    });
+  }
+
+  create(request: Request<any, any, CreateUserBody>, response: Response){ // PayLoad 
+    const { name, email } = request.body;
+
+    Mailservice.sendmail({
+        to: {
+          name,
+          email
+        },
+        message: {
+          subject: "Welcome to Your Rax App",
+          body: `Seja bem-vindo ${name} o seu email cadastrado e ${email}`
+        }
+      }
+    );
+
+    return response.json({
+      name,
+      email
+    });
+  }
+}
+
+export default new UseController();

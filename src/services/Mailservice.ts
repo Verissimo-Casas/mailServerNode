2fc069b74893f972a327b746e1f8bc6366d403d3
Veríssimo Casas <Verissimo0073@gmail.com>
2022-10-03 21:26:11 -0400

    create src/services/Mailservice.ts

---
+++ src/services/Mailservice.ts
@@ -0,0 +1,23 @@
+interface MailTo {
+  name: string;
+  email: string;
+}
+
+interface MailMessage {
+  subject: string;
+  body: string;
+  attachment?: string[] ;
+}
+
+type SendMailType = {
+  to: MailTo;
+  message: MailMessage;
+}
+
+class Mailservice {
+  sendmail({ to, message }: SendMailType) {
+    console.log(`Email enviado para ${to.name} com assunto ${message.subject}, a message ${message.body}`);
+  }
+}
+
+export default new Mailservice();
